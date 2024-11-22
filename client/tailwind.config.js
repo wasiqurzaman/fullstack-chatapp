a10ddb0064bca57f9a074e29b3fa43d7ab25f843
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-11-22 13:53:26 +0530

    add google font to the taliwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        rubik: ["Rubik", "sans-serif"],
+        lato: ["Lato", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
